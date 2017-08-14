1df502ccf6c22bb80197dfe9c2e6b56cd9b21b38
Lakha <polizic@yahoo.co.in>
2017-08-14 13:13:02 +0530

    - Added test case to sort duplicate items

--- test.js
+++ test.js
@@ -11,4 +11,5 @@ let test = (name, result) => {
 
 test('Test1', expect(mergeSort([ 5, 4, 3, 2, 1 ]), [ 1, 2, 3, 4, 5 ]));
 test('Test2', expect(mergeSort([ -5, 4, -3, 2, -1 ]), [ -5, -3, -1, 2, 4 ]));
+test('Test2', expect(mergeSort([ 5, 6, 3, 5, 4, 3, 6, 5 ]), [ 3, 3, 4, 5, 5, 5, 6, 6 ]));
 
